542aa75e311376d501a40ca3d0ffaf1a001ca4b6
rijfas <rijfas01@gmail.com>
2024-08-29 11:27:17 +0530

    fix: added cors

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 const express = require("express");
 const db = require("./db");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use(express.json());
 
 app.post("/blogs", (req, res) => {
